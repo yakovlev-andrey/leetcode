d9ae1c092a3ba330efb4d838fd404644f6dc2b64
Andrey Yakovlev <679008@gmail.com>
2024-09-16 14:23:58 +0000

    2gis interview task

---
+++ interview/2gis.py
@@ -0,0 +1,15 @@
+# Write a function that prints text in the center of a rectangular frame (made of # symbols).
+#The function takes the text, the width, and the height of the rectangle as input parameters.
+#
+# Написать функцию, которая пишет текст в центре прямоугольной рамки (из символов #)
+# В качестве входных параметров - выводимый текст, ширина и высота прямоугольной рамки
+#
+# Пример:
+#
+#   > print_in_box("Hello, World!", 17, 5)
+#
+#   #################
+#   #               #
+#   # Hello, World! #
+#   #               #
+#   #################
