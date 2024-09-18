dfdf8da8f6e38724938f8664c6ae0e5996b11745
Andrey Yakovlev <679008@gmail.com>
2024-09-18 06:36:09 +0000

    Add another solution of the problem Minumum Value to Get Positive Step by Step Sum

--- uncategorized/minimum-value-to-get-positive-step-by-step-sum.py
+++ uncategorized/minimum-value-to-get-positive-step-by-step-sum.py
@@ -1,4 +1,6 @@
+from itertools import accumulate
 from typing import List
+
 
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
@@ -9,9 +11,37 @@ class Solution:
                 min_value = nums[i]
         return 1 if 1 - min_value < 1 else 1 - min_value
 
+
 s = Solution()
 
+assert s.minStartValue([-3, 2, -3, 4, 2]) == 5
+assert s.minStartValue([1, 2]) == 1
+assert s.minStartValue([1, -2, -3]) == 5 
 
-assert s.minStartValue([-3, 2, -3 ,4 , 2]) == 5
+
+class Solution:
+    def minStartValue(self, nums):
+        cur_sum, min_sum = 0, 0
+        for num in nums:
+            cur_sum += num
+            min_sum = min(min_sum, cur_sum)
+        return 1 - min_sum
+
+
+s = Solution()
+
+assert s.minStartValue([-3, 2, -3, 4, 2]) == 5
+assert s.minStartValue([1, 2]) == 1
+assert s.minStartValue([1, -2, -3]) == 5 
+
+
+class Solution:
+    def minStartValue(self, nums):
+        return max(1, 1 - min(accumulate(nums)))
+
+
+s = Solution()
+
+assert s.minStartValue([-3, 2, -3, 4, 2]) == 5
 assert s.minStartValue([1, 2]) == 1
 assert s.minStartValue([1, -2, -3]) == 5
